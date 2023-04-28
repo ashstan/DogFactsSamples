3ebcc6cb7d4771ee4d73441c97ff2c93248518fd
ashstan <ashleystanley.jb@gmail.com>
2023-04-27 23:21:46 -0500

    Add project files.

---
+++ DogFactsSamples/MeFactData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DogFactsSamples
+{
+    public class MeFactData
+    {
+        public MeFactData()
+        {
+        }
+        public static IEnumerable<MeFactData> All { private set; get; }
+        public string TheFact { get; set; }
+        public string ShortFact { get; set; }
+        public string ImgSrc { get; set; }
+        static MeFactData()
+        {
+            List<MeFactData> all = new List<MeFactData>();
+            all.Add(new MeFactData() { TheFact = "My name is Ashley.", ShortFact = "My name", ImgSrc = "name.jpg" });
+            all.Add(new MeFactData() { TheFact = "Never ask a woman her age.", ShortFact = "My age", ImgSrc = "age.jpg" });
+            all.Add(new MeFactData() { TheFact = "I like cats!", ShortFact = "I like", ImgSrc = "cat.jpg" });
+            all.Add(new MeFactData() { TheFact = "I like rock climbing!", ShortFact = "I also like", ImgSrc = "rock.jpg" });
+            all.Add(new MeFactData() { TheFact = "I like to learn!", ShortFact = "Another thing I like", ImgSrc = "learn.jpg" });
+            All = all;
+
+        }
+    }
+}
